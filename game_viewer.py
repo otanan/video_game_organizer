d13d8d738bf4ccce5195698e34247fef534346f4
Jonathan Delgado <jonathandelgado0327@gmail.com>
2019-01-10 01:23:58 -0500

    Change to using simple treeview. Considering adapting to ListView instead

--- game_viewer.py
+++ game_viewer.py
@@ -1,28 +1,39 @@
 """
 ====================================
-Filename:       game_viewer 
+Filename:       game_viewer.py
 Author:         Jonathan Delgado 
 Description:    Module controlling showing the JSON information to screen
                 and creating a GUI for it
 ====================================
 """
-
 from tkinter import *
+from tkinter import ttk
 
 import game_controller
 
 def createWindow():
     root = Tk()
+    treeview = ttk.Treeview(root)
+    treeview.pack()
+
     gamesList = game_controller.readGamesFile()
     #Runs through each game in the games list to create
     #an entry in the GUI
     for game in gamesList:
-        createGameNode(root, game)
+        createGameNode(treeview, game)
 
+    #Start listening for events
+    treeview.bind("<<TreeviewSelect>>", callback)
     root.mainloop()
 
-def createGameNode(root, game):
-    gameLabel = Label(root, text=game.getName()).grid()
+def createGameNode(treeview, game):
+    nodeID = treeview.insert('', '0', text=game.getName())
+    for key in game.getInfo():
+        treeview.insert(nodeID, '0', text= (key + ': ' + str(game.getInfo()[key]) ) )
+
+def callback(event):
+    print("Hello world")
+
 
 def main():
     createWindow()
